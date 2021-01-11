f43a43646c36a614f321d35b9cb1c2017e71c011
Ian <ian@invacto.com>
2021-01-11 18:30:13 +0100

    init

---
+++ README.md
@@ -0,0 +1,36 @@
+# KontrlCMS Skeleton
+
+This is a good base to start off with when you're creating a website.
+
+## Install
+
+```
+git clone https://github.com/ianjaku/kontrlcms-skeleton.git
+```
+
+```
+composer install
+```
+
+
+## Running the php server
+
+You can use the php dev server for local development
+
+```
+cd public
+
+php -S localhost:8000
+```
+
+## Running webpack
+
+This skeleton comes with a fully set up webpack server with an scss parser.
+It's not required and you can just delete it if you don't want it.
+
+```
+cd assets
+
+npm run watch
+```
+
---
+++ src/app.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use invacto\KontrlCMS\KontrlCMS;
+
+$cms = new KontrlCMS(__DIR__ . "/..");
+
+$cms->page("/", "home.twig");
+
+$cms->redirect("/login", "/simplecms/login");
+
+$cms->addPlugin(new \invacto\KontrlCMS\plugins\text\TextPlugin());
+//$cms->addPlugin(new \invacto\KontrlCMS\plugins\img\ImgPlugin());
+//$cms->addPlugin(new \invacto\KontrlCMS\plugins\bg_img\BGImgPlugin());
+//$cms->addPlugin(new \invacto\KontrlCMS\plugins\wysiwyg\WYSIWYGPlugin());
+//$cms->addPlugin(new \invacto\KontrlCMS\plugins\page_settings\PageSettingsPlugin());
+//$cms->addPlugin(new \invacto\KontrlCMS\plugins\posts\PostsPlugin([
+//	[
+//		"name" => "blog",
+//		"view" => "post.twig",
+//		"slug" => "post_title"
+//	]
+//]));
+
+$cms->run();
+
